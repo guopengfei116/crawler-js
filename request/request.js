a5011a7e48928a245da4e6b3f3fc48b1d063978d
PengfeiGuo <pengfeiguo@guopengfeideMacBook-Pro.local>
2017-12-18 01:28:53 +0800

    并发请求基本实现

---
+++ request/request.js
@@ -0,0 +1,51 @@
+/**
+ * GET请求
+ * @params { url: String || Object } url或配置对象
+ * @params { retry: Number } 失败后重试次数
+ * @params config { url: String } url
+ * @params config { retry: Number } 失败后重试次数
+ * @return Promise.Any 默认返回rsp对象，遇到失败时返回含有url信息的对象
+ */
+async function request(url, retry) {
+    let config = null;
+    url = typeof url === 'string'? url: ((config = url) && config.url);
+    retry = typeof retry === 'number'? retry: (config.retry || 1);
+
+    // 成功返回rsp, 失败则重试, 
+    try {
+        return await axios.get(url);         // 必须使用await才可用try捕获失败
+    }catch(e) {
+        if(retry >= 1) {
+            return get(url, retry - 1);
+        }else {
+            return Promise.reject({e, url}); // 重试失败后返回错误对象与url
+        }
+    }
+};
+
+/**
+ * GET并发请求
+ * @params { config: Object } 参数配置
+ * @params { config.urls: Array } url列表
+ * @params { config.concurrent: Number } 并发量
+ * @params { config.retry: Number } 错误重试次数
+ * @params { config.tranformSingle: Function.optional } 数据转换的钩子函数，传入response对象, 要求返回新的itemData
+ * @params { config.tranformAll: Function.optional } 数据转换的钩子函数，传入所有itemData构成的数组, 要求返回新的resultData
+ * @return Promise.Any 默认会返回一个数组, 也可通过钩子函数设定最终的返回结果
+ */
+async function requestConcurrent(config) {
+    let { urls, retry, concurrent, tranformSingle, tranformAll } = config;
+    let tempUrls = null, result = [];
+
+    while((tempUrls = urls.slice(currentIndex, currentIndex + concurrent)) && tempUrls.length) {
+        result.push(await Promise.all(tempUrls.map(v => request(url))))
+    }
+
+    return result;
+};
+
+// 导出两个请求方法
+module.exports = {
+    request,
+    requestConcurrent
+};
